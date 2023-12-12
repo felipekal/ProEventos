4e8702049800bc58bcf872ec1e60158a75e5360b
Felipe Kal <felipe.kal@outlook.com>
2023-12-12 19:56:37 -0300

    Secao 5 - Interpolacao, Diretivas e Biding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltered: any = [];
+  widthImg: number = 250;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList() {return this._filterList}
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.eventosFiltered = this.filterList ? this.filterEvents(this.filterList) : this.eventos
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (      evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 || evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +35,18 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  displayImg(){
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(response => this.eventos = response, error => console.log(error));
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response =>
+        {
+          this.eventos = response
+          this.eventosFiltered = this.eventos;
+        },
+        error => console.log(error)
+    );
   }
 }
